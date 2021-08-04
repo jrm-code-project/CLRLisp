bfd74940dd42d7b30317d01b0dc1ed36da1fbcc7
Joe Marshall <bitdiddle@hotmail.com>
2021-08-04 13:40:43 -0700

    Initial commit.

---
+++ LispLib/Reader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispLib
+{
+    public class Reader
+    {
+        public static object Read () {
+            throw new NotImplementedException ();
+        }
+    }
+}
